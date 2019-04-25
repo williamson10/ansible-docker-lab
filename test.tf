974598804276c54e6e2c60ce8d548b3b3a586586
Justin Williamson <williamson10@gmail.com>
2019-04-25 12:07:19 -0400

    Add code-server-jekyll

---
+++ test.tf
@@ -0,0 +1,37 @@
+
+provider "docker" {
+  host = "tcp://docker:2375/"
+
+  registry_auth {
+    address = "registry.hub.docker.com"
+    config_file = "config.json"
+  }
+}
+
+data "docker_registry_image" "code-server-jekyll" {
+  name = "williamson10/code-server-jekyll"
+}
+
+resource "docker_image" "code-server-jekyll" {
+  name          = "${data.docker_registry_image.code-server-jekyll.name}"
+  pull_triggers = ["${data.docker_registry_image.code-server-jekyll.sha256_digest}"]
+}
+
+resource "docker_container" "code-server-jekyll" {
+  name = "code-server-jekyll"
+  image = "${docker_image.code-server-jekyll.name}"
+  ports {
+    internal = 8443
+    external = 8443
+  }
+  ports {
+    internal = 4000
+    external = 4000
+  }
+  volumes {
+    volume_name = "code-server_data"
+    container_path = "/home/coder/project"
+  }
+  env = ["PASSWORD=letmein"] # TODO: Look into vault
+ 
+}
